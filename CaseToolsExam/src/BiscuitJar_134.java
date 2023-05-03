06ab40067dedda36ef7c91b8fbba351b02bbe783
exam <exam@Bcalab4-m12>
2023-05-03 12:29:01 +0530

    commit

---
+++ CaseToolsExam/src/BiscuitJar_134.java
@@ -0,0 +1,15 @@
+
+public class BiscuitJar_134 {
+
+	int num_biscuits;
+	
+	BiscuitJar_134(int num_biscuits)
+	{
+		this.num_biscuits=num_biscuits;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
